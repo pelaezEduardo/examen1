d072d9778457143a0dee727ec8e1f9ca894a09c0
Víctor Orozco <vorozco@nabenik.com>
2017-09-11 08:55:07 -0600

    Adding eclipse facets

---
+++ src/main/java/gt/edu/url/examen1/api/Chudnovsky.java
@@ -0,0 +1,11 @@
+package gt.edu.url.examen1.api;
+
+/**
+ *
+ * @author tuxtor
+ */
+public interface Chudnovsky {
+    
+    public double calcularPi(int cantidadDigitos);
+    
+}
---
+++ src/main/java/gt/edu/url/examen1/api/Reportero.java
@@ -0,0 +1,11 @@
+package gt.edu.url.examen1.api;
+
+/**
+ *
+ * @author tuxtor
+ */
+public interface Reportero {
+    	public String calcularGanador(int a, int b);
+	public String calcularCampeon();
+	public String recordarGanador(int juego);
+}
