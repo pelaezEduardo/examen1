56730abf2b1346ed9cf558b503dfd08fab6c22c3
Víctor Orozco <vorozco@nabenik.com>
2017-09-17 17:54:13 -0600

    Adding reporter unit test

--- src/main/java/gt/edu/url/examen1/impl/ReporteroImpl.java
+++ src/main/java/gt/edu/url/examen1/impl/ReporteroImpl.java
@@ -1,45 +1,74 @@
 package gt.edu.url.examen1.impl;
 
 import gt.edu.url.examen1.api.Reportero;
+import gt.edu.url.examen1.ds.ArrayList;
 
 /**
  *
  * @author tuxtor
  */
 public class ReporteroImpl implements Reportero{
+    
+    ArrayList<ResultadoJuego> resultados = new ArrayList<>();
 
     @Override
     public String calcularGanador(int a, int b) {
         if(a > 7 || b > 7)
             return "Resultado invalido";
         
+        //Validacion por inclusion
         if( (a == 7 || b == 7) && Math.abs(a-b) > 2){
             return "Resultado invalido";
         }else if(a == 7){
+            resultados.add(0, new ResultadoJuego(a, b));
             return "A";
         }else if(b == 7){
+            resultados.add(0, new ResultadoJuego(a, b));
             return "B";
         }
         
+        //Validacion por exclusion
         if (a-b >= 2 && a == 6){
+            resultados.add(0, new ResultadoJuego(a, b));
             return "A";
         }else if(b-a >= 2 && b == 6){
+            resultados.add(0, new ResultadoJuego(a, b));
             return "B";
         }else{
             return "Resultado invalido";
         }
-        
-        //Save in queue
     }
 
     @Override
     public String calcularCampeon() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int juegosa=0, juegosb=0;
+        for(int i = 0; i < resultados.size() ; i ++){
+            ResultadoJuego res = resultados.get(i);
+            if(res != null){
+                if(res.getA() > res.getB()){
+                    juegosa++;
+                }else{
+                    juegosb++;
+                }
+            }
+        }
+        if(juegosa > juegosb){
+            return "A";
+        }else if(juegosa < juegosb){
+            return "B";
+        }else{
+            return "Empate";
+        }
     }
 
     @Override
     public String recordarGanador(int juego) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ResultadoJuego res = resultados.get(juego);
+        if(res != null){
+            return res.toString();
+        }else{
+            return "Juego invalido";
+        }
     }
     
     
--- src/test/java/gt/edu/url/examen1/ReporteroUnitTest.java
+++ src/test/java/gt/edu/url/examen1/ReporteroUnitTest.java
@@ -32,7 +32,6 @@ public class ReporteroUnitTest {
         
     }
     
-    
     @Test
     public void testInvalidScores() {
         Reportero repo = new ReporteroImpl();
@@ -44,6 +43,26 @@ public class ReporteroUnitTest {
         assertNotEquals("B", repo.calcularGanador(0, 21));
         assertNotEquals("B", repo.calcularGanador(0, 7));
         assertNotEquals("B", repo.calcularGanador(1, 5));
+    }
+    
+    
+    @Test
+    public void testCampeonato() {
+        Reportero repo = new ReporteroImpl();
+        //Resultados validos
+        repo.calcularGanador(7, 5);
+        repo.calcularGanador(6, 7);
+        repo.calcularGanador(3, 6);
+        repo.calcularGanador(1, 6);
+        //B es el campeon
+        assertEquals(repo.calcularCampeon(), "B");
+        
+        repo.calcularGanador(6, 2);
+        repo.calcularGanador(6, 1);
+        
+        //Empate, no hay ganador
+        assertNotEquals(repo.calcularCampeon(), "B");
+        
         
     }
 }
